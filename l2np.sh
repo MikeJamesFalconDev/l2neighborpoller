39bfd769e0d5e8504f70373323a59307ac4fc141
Mike James <mike.james@falcondev.tech>
2025-01-10 14:26:52 -0300

    Added runner script

---
+++ l2np.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RUNNING_DIR=`dirname $0`
+cd $RUNNING_DIR
+VENV=$RUNNING_DIR/venv
+
+if [ ! -d $VENV ];then
+	python -m venv $VENV
+	source $VENV/bin/activate
+	pip install -r ./requirements.txt
+else
+	source $VENV/bin/activate
+fi
+
+python $RUNNING_DIR/src/l2np.py
