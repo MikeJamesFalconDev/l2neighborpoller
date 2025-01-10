baa1f604d23c1a9cb3cb4aaefba541d148129e17
Mike James <mike.james@falcondev.tech>
2025-01-10 16:43:34 -0300

    Fixed bug setting SNMP version

--- src/l2_snmp.py
+++ src/l2_snmp.py
@@ -8,7 +8,7 @@ def get_oid(oids, host):
         raise Exception(f'Invalid snmp version {config["version"]}. Currently supported 1 and 2c')
     value = None
     with Engine() as engine:
-        version = SNMPv2c if config['version'] != '2c' else SNMPv1
+        version = SNMPv2c if config['version'] == '2c' else SNMPv1
         host = engine.Manager(host, version=version)
         for oid in oids:
             try:
